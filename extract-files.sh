93ae340eb925a7894b46df440cfc7a6dc92658a1
dhacker29 <dhackerdvm@gmail.com>
2014-05-25 01:28:06 -0400

    extract-files: update for new setup-makefiles
    Change-Id: Id8779f2e01986a8a601b8dfca3c18eb9fb0e2143

--- extract-files.sh
+++ extract-files.sh
@@ -2,5 +2,9 @@
 
 export VENDOR=motorola
 export DEVICE=falcon
+# Comment these out if there is no common device tree for this device
+export COMMON_DEVICE=titan
+export BOARD_VENDOR=motorola-qcom
+export TARGET_BOARD_PLATFORM=msm8226
 
-../titan/extract-files.sh $@
+../$COMMON_DEVICE/extract-files.sh $@
