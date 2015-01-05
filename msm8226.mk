a3049979b90d36e1501315f7007a0dd4e600093e
Simon Sickle <simon@simonsickle.com>
2015-01-05 04:10:16 +0000

    Revert "msm8266-common: Vendor files should be included if-exists"
    This reverts commit 72d35461fbb38c4943eeb807c214cdb056b842e8.
    
    Change-Id: I0b1a31ad05f53aa8d3806a595c17fb3de338cb37

--- msm8226.mk
+++ msm8226.mk
@@ -16,7 +16,7 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product-if-exists, vendor/motorola/titan/titan-vendor.mk)
+$(call inherit-product, vendor/motorola/titan/titan-vendor.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
