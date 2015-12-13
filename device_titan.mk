d104271e4ff32d9343ce0a318085cd36eae201f6
LuK1337 <priv.luk@gmail.com>
2015-12-13 13:54:50 +0100

    titan: sensors: bst: Import crackling's sensor HAL
    Change-Id: I210c49d72767610ef103ac09dc5bf415104be4bf

--- device_titan.mk
+++ device_titan.mk
@@ -109,3 +109,16 @@ PRODUCT_PACKAGES += \
     libqsap_sdk \
     libQWiFiSoftApCfg \
     wcnss_service
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
+
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.msm8226 \
+    sensord
